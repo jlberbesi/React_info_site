09dc4c36fa66b8ade7687b700352308e47b44862
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-04-15 22:11:26 -0500

    Add markup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+  <div>
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
 
-//ReactDOM.render(navbar, document.getElementById("root"))
-ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+ReactDOM.render(page, document.getElementById("root"))
+
